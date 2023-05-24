ecdb307deeed1e1192e9ca11d3dbb9c02dfe42d6
Logan Walker <logantwalker95@gmail.com>
2023-05-24 13:44:27 -0400

    tweaking some hyperparams to mimic nanoGPT

--- gpt.py
+++ gpt.py
@@ -11,20 +11,20 @@ batch_size = 64 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 3e-4
+learning_rate = 6e-4
 device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
-dropout = 0.2
+n_embd = 768
+n_head = 12
+n_layer = 12
+dropout = 0.0
 vocab_size = 2000
 # ------------
 
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('simplewiki.txt', 'r', encoding='utf-8') as f:
+with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # create a mapping from characters to integers
