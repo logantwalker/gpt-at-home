f6aff161da6a3620bc493b1df2973c4ba32d20b1
Logan Walker <logantwalker95@gmail.com>
2023-05-25 13:56:47 -0400

    reducing number of tokens generated from the model

--- gpt.py
+++ gpt.py
@@ -265,5 +265,5 @@ for iter in range(start_iter, max_iters):
 
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=50)[0].tolist()))
 #open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
