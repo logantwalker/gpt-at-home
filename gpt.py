47745fa533e7f21ce521317aecf44eeb3804fb7f
Logan Walker <logantwalker95@gmail.com>
2023-05-25 12:18:01 -0400

    trying out the hugging face datasets

--- gpt.py
+++ gpt.py
@@ -27,6 +27,12 @@ torch.manual_seed(1337)
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
+
+from datasets import load_dataset
+
+dataset = load_dataset('tiny_shakespeare')
+
+
 # create a mapping from characters to integers
 os.makedirs('./models', exist_ok=True)
 if not os.path.isfile('./models/tokenizer.model'):
@@ -36,35 +42,77 @@ sp.load("./models/tokenizer.model")
 encode = lambda s: sp.encode_as_ids(s)  # encoder: take a string, output a list of integers
 decode = lambda l: sp.decode_ids(l)  # decoder: take a list of integers, output a string
 
+
+# encoding hugging face dataset
+encoded_dataset = dataset.map(lambda examples: {'input_ids': encode(examples['text'])}, batched=True, remove_columns=['text'])
+
+
 # Train and test splits
-data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9*len(data)) # first 90% will be train, rest val
-train_data = data[:n]
-val_data = data[n:]
+train_dataset = encoded_dataset['train'].shuffle(seed=42)
+val_dataset = encoded_dataset['validation']
+# data = torch.tensor(encode(text), dtype=torch.long)
+# n = int(0.9*len(data)) # first 90% will be train, rest val
+# train_data = data[:n]
+# val_data = data[n:]
+
+
+from torch.utils.data import DataLoader
+# Create data loaders for training and validation sets
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
 
 # data loading
-def get_batch(split):
+# def get_batch(split):
+#     # generate a small batch of data of inputs x and targets y
+#     data = train_data if split == 'train' else val_data
+#     ix = torch.randint(len(data) - block_size, (batch_size,))
+#     x = torch.stack([data[i:i+block_size] for i in ix])
+#     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+#     x, y = x.to(device), y.to(device)
+#     return x, y
+
+def get_batch(dataset, block_size, batch_size, device):
     # generate a small batch of data of inputs x and targets y
-    data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    x, y = x.to(device), y.to(device)
+    x = torch.zeros((batch_size, block_size), dtype=torch.long, device=device)
+    y = torch.zeros((batch_size, block_size), dtype=torch.long, device=device)
+    for i in range(batch_size):
+        try:
+            example = next(dataset)
+        except StopIteration:
+            # if we've reached the end of the dataset, reshuffle and restart the iterator
+            dataset = iter(dataset.shuffle(seed=42))
+            example = next(dataset)
+        x[i, :] = example["input_ids"][:-1]
+        y[i, :] = example["input_ids"][1:]
     return x, y
 
 @torch.no_grad()
 def estimate_loss():
     out = {}
     model.eval()
-    for split in ['train', 'val']:
-        losses = torch.zeros(eval_iters)
+    for split, dataset in [("train", train_dataset), ("val", val_dataset)]:
+        dataset_iter = iter(dataset)
+        losses = torch.zeros(eval_iters, device=device)
         for k in range(eval_iters):
-            X, Y = get_batch(split)
+            X, Y = get_batch(dataset_iter, block_size, batch_size, device)
             logits, loss = model(X, Y)
             losses[k] = loss.item()
-        out[split] = losses.mean()
+        out[split] = losses.mean().item()
     model.train()
     return out
+# @torch.no_grad()
+# def estimate_loss():
+#     out = {}
+#     model.eval()
+#     for split in ['train', 'val']:
+#         losses = torch.zeros(eval_iters)
+#         for k in range(eval_iters):
+#             X, Y = get_batch(split)
+#             logits, loss = model(X, Y)
+#             losses[k] = loss.item()
+#         out[split] = losses.mean()
+#     model.train()
+#     return out
 
 class Head(nn.Module):
     """ one head of self-attention """
